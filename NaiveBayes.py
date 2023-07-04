a4ae5748a407963932b671f15c47a497585e2f96
Youssef Aghzere <youssef.aghzere@ecole.ensicaen.fr>
2023-07-04 19:52:10 +0200

    Initial commit

---
+++ NaiveBayes.py
@@ -0,0 +1,131 @@
+# Import libraries
+import os
+import glob
+import pandas as pd
+import numpy as nm
+import matplotlib.pyplot as mtp
+import nltk
+from nltk.corpus import stopwords
+import string
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import pickle
+
+
+
+def readFile(filepath):
+    with open(filepath, "r") as df:
+        content = df.read()
+    return content
+
+
+def dir_to_csv(folder, ai_generated): # ai_generated = 0 or 1
+    txt_files = glob.glob(folder + '/*.txt')
+    texts_list = []
+    for file_path in txt_files:
+        texts_list.append(readFile(file_path))
+
+    texts_dataframe = pd.DataFrame({'Text' : texts_list, 'AI-Generated': [ai_generated for i in range(len(texts_list))]})
+    texts_dataframe.to_csv(f'{folder}.csv', index=False)
+
+def combine_csv_files(parent_folder, filename):
+    csv_files = glob.glob(parent_folder + '/*.csv')
+    dfs = []
+    for file in csv_files:
+        df = pd.read_csv(file)
+        dfs.append(df)
+    merged_df = pd.concat(dfs)
+    shuffled_df = merged_df.sample(frac=1)
+    shuffled_df.to_csv(f'{parent_folder}/{filename}_ai_human_merged.csv', index=False)
+
+# csv files creation and combination
+"""root_dir = '/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author'
+authors_dirs = glob.glob(root_dir + "/*")
+for author_dir in authors_dirs:
+    author_basename = author_dir.split("/")[-1]
+    ai_dir = author_dir + f"/{author_basename}_AI"
+    human_dir = author_dir + f"/{author_basename}_Human"
+    csv_ai_file = dir_to_csv(ai_dir, 1)
+    csv_human_file = dir_to_csv(human_dir, 0)
+    combine_csv_files(author_dir, author_basename)
+    os.remove(f"{author_dir}/{author_basename}_AI.csv")
+    os.remove(f"{author_dir}/{author_basename}_Human.csv")"""
+
+
+def process_text(text):
+  # Remove punctuation
+  nopunc = [char for char in text if char not in string.punctuation]
+  nopunc = ''.join(nopunc)
+
+  # Remove stopwords
+  clean_words = [word for word in nopunc.split() if word not in stopwords.words('english')]
+  return clean_words
+
+
+def naive_bayes(csv_file):
+    dataset = pd.read_csv(csv_file)
+
+    # Remove rows containing NaN values
+    dataset.dropna(inplace=True)
+
+    # Convert a collection of text to a matrix of tokens
+    vectorizer = CountVectorizer(analyzer=process_text)
+    text_bow = vectorizer.fit_transform(dataset['Text'])
+    x = dataset.iloc[:, 0]
+    y = dataset.iloc[:, 1]
+
+    # splitting the dataset into the training set and test set
+    x_train, x_test, y_train, y_test = train_test_split(text_bow, y, test_size = 0.2, random_state = 0)
+
+    # Fitting Naive Bayes to the Training set
+    classifier = MultinomialNB().fit(x_train, y_train)
+
+    # Evaluate the model of the training dataset
+    pred = classifier.predict(x_train)
+    print("--- Evaluation of the classifier on the training dataset ---")
+    print(classification_report(y_train, pred))
+    print()
+    print('Confusion Matrix :\n', confusion_matrix(y_train, pred))
+    print()
+    print('Accuracy : ', accuracy_score(y_train, pred))
+
+    # Evaluate the model of the testing dataset
+    print("--- Evaluation of the classifier on the test dataset ---")
+    pred_test = classifier.predict(x_test)
+    print(classification_report(y_test, pred_test))
+    print()
+    print('Confusion Matrix :\n', confusion_matrix(y_test, pred_test))
+    print()
+    print('Accuracy : ', accuracy_score(y_test, pred_test))
+
+    return vectorizer, classifier
+
+
+
+def save_classifier_and_vectorizer(csv_file, destination_folder):
+    vectorizer, classifier = naive_bayes(csv_file)
+    with open(f'{destination_folder}/classifier.pickle', 'wb') as f:
+        pickle.dump(classifier, f)
+    with open(f'{destination_folder}/vectorizer.pickle', 'wb') as f:
+        pickle.dump(vectorizer, f)
+
+
+def read_classifier_and_vectorizer():
+    with open('classifier.pickle', 'rb') as f:
+        classifier = pickle.load(f)
+    with open('vectorizer.pickle', 'rb') as f:
+        vectorizer = pickle.load(f)
+
+    return vectorizer, classifier
+
+
+root_dir = '/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author'
+authors_dirs = glob.glob(root_dir + "/*")
+for author_dir in authors_dirs:
+    author_basename = author_dir.split("/")[-1]
+    print(f"***************** {author_basename}*****************")
+    csv_file = f"{author_dir}/{author_basename}_ai_human_merged.csv"
+    save_classifier_and_vectorizer(csv_file, author_dir)
+    print()
---
+++ subdatasets_structure.py
@@ -0,0 +1,40 @@
+import glob
+import os
+import shutil
+
+folder = '/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author'
+authors_dirs = glob.glob(folder + "/*")
+"""print(authors_dirs)
+
+
+for dir in authors_dirs:
+    basename = dir.split("/")[-1]
+    os.makedirs(f"{dir}/{basename}_Human")
+    os.makedirs(f"{dir}/{basename}_AI")
+"""
+
+"""datasets_folders = ['/home/youssef/Desktop/classification_algorithms_per_author/DATASETS/Reedsy_Prompts_short_stories',
+                    '/home/youssef/Desktop/classification_algorithms_per_author/DATASETS/Reedsy_Prompts_short_stories_AI_Human_Style',
+                    '/home/youssef/Desktop/classification_algorithms_per_author/DATASETS/Reedsy_Prompts_short_stories_AI_Person_style',
+                    '/home/youssef/Desktop/classification_algorithms_per_author/DATASETS/Reedsy_Prompts_short_stories_AI_version',
+                    '/home/youssef/Desktop/classification_algorithms_per_author/DATASETS/Stories_AI_person_style']
+folder = datasets_folders[4]
+authors_folders = glob.glob(folder + "/*")
+for f in authors_folders:
+        basename = f.split("/")[-1]
+        txt_files = glob.glob(f + "/*.txt")
+        for file in txt_files:
+            shutil.copy(file, f"/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author/{basename}/{basename}_AI/{file.split('/')[-1]}")
+
+"""
+datasets_folders = ['/home/youssef/Desktop/classification_algorithms_per_author/DATASETS/Stories_AI',
+                    '/home/youssef/Desktop/classification_algorithms_per_author/DATASETS/Stories_AI_human_style']
+
+folder = datasets_folders[1]
+for f in glob.glob('/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author/*'):
+    basename = f.split("/")[-1]
+    txt_files = glob.glob(folder + "/*.txt")
+    for file in txt_files:
+        shutil.copy(file, f"/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author/{basename}/{basename}_AI/{file.split('/')[-1]}")
+
+
