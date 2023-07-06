93883a56b1984437e6ee45be61c2ffcdaa4850e0
Youssef Aghzere <youssef.aghzere@ecole.ensicaen.fr>
2023-07-06 10:31:48 +0200

    authors' related stories structuration

--- subdatasets_structure.py
+++ subdatasets_structure.py
@@ -2,17 +2,22 @@ import glob
 import os
 import shutil
 
-folder = '/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author'
+"""folder = '/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author'
 authors_dirs = glob.glob(folder + "/*")
-"""print(authors_dirs)
+print(authors_dirs)
 
 
 for dir in authors_dirs:
     basename = dir.split("/")[-1]
-    os.makedirs(f"{dir}/{basename}_Human")
-    os.makedirs(f"{dir}/{basename}_AI")
-"""
+    os.makedirs(f"{dir}/{basename}_Reedsy_Prompts_short_stories")
+    os.makedirs(f"{dir}/{basename}_Reedsy_Prompts_short_stories_AI_Human_Style")
+    os.makedirs(f"{dir}/{basename}_Reedsy_Prompts_short_stories_AI_Person_style")
+    os.makedirs(f"{dir}/{basename}_Reedsy_Prompts_short_stories_AI_version")
+    os.makedirs(f"{dir}/{basename}_Stories_AI")
+    os.makedirs(f"{dir}/{basename}_Stories_AI_human_style")
+    os.makedirs(f"{dir}/{basename}_Stories_AI_person_style")
 
+"""
 """datasets_folders = ['/home/youssef/Desktop/classification_algorithms_per_author/DATASETS/Reedsy_Prompts_short_stories',
                     '/home/youssef/Desktop/classification_algorithms_per_author/DATASETS/Reedsy_Prompts_short_stories_AI_Human_Style',
                     '/home/youssef/Desktop/classification_algorithms_per_author/DATASETS/Reedsy_Prompts_short_stories_AI_Person_style',
@@ -24,7 +29,7 @@ for f in authors_folders:
         basename = f.split("/")[-1]
         txt_files = glob.glob(f + "/*.txt")
         for file in txt_files:
-            shutil.copy(file, f"/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author/{basename}/{basename}_AI/{file.split('/')[-1]}")
+            shutil.copy(file, f"/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author/{basename}/{basename}_Stories_AI_person_style/{file.split('/')[-1]}")
 
 """
 datasets_folders = ['/home/youssef/Desktop/classification_algorithms_per_author/DATASETS/Stories_AI',
@@ -35,6 +40,4 @@ for f in glob.glob('/home/youssef/Desktop/classification_algorithms_per_author/D
     basename = f.split("/")[-1]
     txt_files = glob.glob(folder + "/*.txt")
     for file in txt_files:
-        shutil.copy(file, f"/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author/{basename}/{basename}_AI/{file.split('/')[-1]}")
-
-
+        shutil.copy(file, f"/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author/{basename}/{basename}_Stories_AI_human_style/{file.split('/')[-1]}")
