7679a22e251d492f1109d336bd5ed3175a513792
Youssef Aghzere <youssef.aghzere@ecole.ensicaen.fr>
2023-07-06 17:10:13 +0200

    Naive bayes algorithm's implementation that allow to dispatch with effectiveness dataset of each author (17 authors) into train dataset and test dataset

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -21,13 +21,13 @@ def readFile(filepath):
     return content
 
 
-def dir_to_csv(folder, ai_generated): # ai_generated = 0 or 1
+def dir_to_csv(folder, ai_generated, writing_style): # ai_generated = 0 or 1
     txt_files = glob.glob(folder + '/*.txt')
     texts_list = []
     for file_path in txt_files:
         texts_list.append(readFile(file_path))
 
-    texts_dataframe = pd.DataFrame({'Text' : texts_list, 'AI-Generated': [ai_generated for i in range(len(texts_list))]})
+    texts_dataframe = pd.DataFrame({'Text' : texts_list, 'Writing Style': [writing_style for i in range(len(texts_list))], 'AI-Generated': [ai_generated for i in range(len(texts_list))]})
     texts_dataframe.to_csv(f'{folder}.csv', index=False)
 
 def combine_csv_files(parent_folder, filename):
@@ -45,15 +45,34 @@ def combine_csv_files(parent_folder, filename):
 authors_dirs = glob.glob(root_dir + "/*")
 for author_dir in authors_dirs:
     author_basename = author_dir.split("/")[-1]
-    ai_dir = author_dir + f"/{author_basename}_AI"
-    human_dir = author_dir + f"/{author_basename}_Human"
-    csv_ai_file = dir_to_csv(ai_dir, 1)
-    csv_human_file = dir_to_csv(human_dir, 0)
+
+
+    ai_dir = author_dir + f"/{author_basename}_Reedsy_Prompts_short_stories_AI_version"
+    aip_dir = author_dir + f"/{author_basename}_Reedsy_Prompts_short_stories_AI_Person_style"
+    aih_dir = author_dir + f"/{author_basename}_Reedsy_Prompts_short_stories_AI_Human_Style"
+    sai_dir = author_dir + f"/{author_basename}_Stories_AI"
+    saih_dir = author_dir + f"/{author_basename}_Stories_AI_human_style"
+    saip_dir = author_dir + f"/{author_basename}_Stories_AI_person_style"
+    human_dir = author_dir + f"/{author_basename}_Reedsy_Prompts_short_stories"
+
+
+    dir_to_csv(ai_dir, 1, 'ReedSy-AI')
+    dir_to_csv(aip_dir, 1, 'ReedSy-AI-Person-Style')
+    dir_to_csv(aih_dir, 1, 'ReedSy-AI-Human-Style')
+    dir_to_csv(sai_dir, 1, 'Stories-AI')
+    dir_to_csv(saih_dir, 1, 'Stories-AI-Human-Style')
+    dir_to_csv(saip_dir, 1, 'Stories-AI-Person-Style')
+    dir_to_csv(human_dir, 0, 'ReedSy-Human')
+
     combine_csv_files(author_dir, author_basename)
-    os.remove(f"{author_dir}/{author_basename}_AI.csv")
-    os.remove(f"{author_dir}/{author_basename}_Human.csv")"""
-
-
+    os.remove(f"{author_dir}/{author_basename}_Stories_AI_person_style.csv")
+    os.remove(f"{author_dir}/{author_basename}_Stories_AI_human_style.csv")
+    os.remove(f"{author_dir}/{author_basename}_Stories_AI.csv")
+    os.remove(f"{author_dir}/{author_basename}_Reedsy_Prompts_short_stories_AI_version.csv")
+    os.remove(f"{author_dir}/{author_basename}_Reedsy_Prompts_short_stories_AI_Person_style.csv")
+    os.remove(f"{author_dir}/{author_basename}_Reedsy_Prompts_short_stories_AI_Human_Style.csv")
+    os.remove(f"{author_dir}/{author_basename}_Reedsy_Prompts_short_stories.csv")
+"""
 def process_text(text):
   # Remove punctuation
   nopunc = [char for char in text if char not in string.punctuation]
@@ -72,12 +91,23 @@ def naive_bayes(csv_file):
 
     # Convert a collection of text to a matrix of tokens
     vectorizer = CountVectorizer(analyzer=process_text)
-    text_bow = vectorizer.fit_transform(dataset['Text'])
-    x = dataset.iloc[:, 0]
-    y = dataset.iloc[:, 1]
 
-    # splitting the dataset into the training set and test set
-    x_train, x_test, y_train, y_test = train_test_split(text_bow, y, test_size = 0.2, random_state = 0)
+    unique_styles = dataset['Writing Style'].unique()
+
+    test_data = pd.DataFrame(columns=dataset.columns)
+    train_data = pd.DataFrame(columns=dataset.columns)
+
+    for style in unique_styles:
+        style_data = dataset[dataset['Writing Style'] == style]
+        style_train, style_test = train_test_split(style_data, test_size=0.2, random_state=0)
+        test_data = pd.concat([test_data, style_test])
+        train_data = pd.concat([train_data, style_train])
+
+    x_train = vectorizer.fit_transform(train_data['Text'])  # Transform the training text data
+    y_train = train_data['AI-Generated'].astype(int)
+
+    x_test = vectorizer.transform(test_data['Text'])  # Transform the testing text data
+    y_test = test_data['AI-Generated'].astype(int)
 
     # Fitting Naive Bayes to the Training set
     classifier = MultinomialNB().fit(x_train, y_train)
@@ -111,13 +141,45 @@ def read_classifier_and_vectorizer():
 
     return vectorizer, classifier
 
-
 root_dir = '/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author'
 classifiers_dir = '/home/youssef/Desktop/classification_algorithms_per_author/classifiers'
 authors_dirs = glob.glob(root_dir + "/*")
 for author_dir in authors_dirs:
     author_basename = author_dir.split("/")[-1]
-    print(f"***************** {author_basename}*****************")
+    print(f"***************** {author_basename} *****************")
     csv_file = f"{author_dir}/{author_basename}_ai_human_merged.csv"
     save_classifier_and_vectorizer(csv_file, f"{classifiers_dir}/{author_basename}", author_basename)
     print()
+
+
+
+"""
+dataset = pd.read_csv("/home/youssef/Desktop/classification_algorithms_per_author/Datasets_per_author/Aries_Walker/Aries_Walker_ai_human_merged.csv")
+
+# Remove rows containing NaN values
+dataset.dropna(inplace=True)
+
+# Convert a collection of text to a matrix of tokens
+vectorizer = CountVectorizer(analyzer=process_text)
+# text_bow = vectorizer.fit_transform(dataset['Text'])
+
+unique_styles = dataset['Writing Style'].unique()
+
+test_data = pd.DataFrame(columns=dataset.columns)
+train_data = pd.DataFrame(columns=dataset.columns)
+
+for style in unique_styles:
+    style_data = dataset[dataset['Writing Style'] == style]
+    style_train, style_test = train_test_split(style_data, test_size=0.2, random_state=0)
+    test_data = pd.concat([test_data, style_test])
+    train_data = pd.concat([train_data, style_train])
+
+x_train = vectorizer.fit_transform(train_data['Text'])  # Transform the training text data
+y_train = train_data['AI-Generated']
+
+x_test = vectorizer.transform(test_data['Text'])  # Transform the testing text data
+y_test = test_data['AI-Generated']
+
+print(y_test)
+
+"""
